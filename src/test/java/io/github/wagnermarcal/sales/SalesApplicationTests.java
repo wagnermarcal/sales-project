36ee351423842b26b76945fe2c5a6461384befae
Wagner Marçal <wagnermarcal.2008@gmail.com>
2022-01-07 11:03:08 -0300

    Initial files

---
+++ src/test/java/io/github/wagnermarcal/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wagnermarcal.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
